b83002eb84d18278cb686dda516d7e47a08208dc
Balie Shalomi Pacholi <31888480+balipacholi@users.noreply.github.com>
2025-04-13 11:04:54 +0530

    Update main.cpp

--- Abstract Factory Design Pattern/main.cpp
+++ Abstract Factory Design Pattern/main.cpp
@@ -4,92 +4,114 @@
 
 using namespace std;
 
-class IButton{
-	public:
-	  virtual void press() = 0;
+class IButton
+{
+public:
+   virtual void press() = 0;
 };
 
-class ITextBox{
-	public:
-	  virtual void setetxt() = 0;
+class ITextBox
+{
+public:
+   virtual void setetxt() = 0;
 };
 
-class MacButton : public IButton {
-	public:
-	  void press() {
-	     std::cout << "mac button created" << std::endl;
-	  }
+class MacButton : public IButton
+{
+public:
+   void press()
+   {
+      std::cout << "mac button created" << std::endl;
+   }
 };
 
-class WinButton : public IButton {
-	public:
-	  void press() {
-	     std::cout << "Win button created" << std::endl;
-	  }
+class WinButton : public IButton
+{
+public:
+   void press()
+   {
+      std::cout << "Win button created" << std::endl;
+   }
 };
 
-class MacTextBox : public ITextBox {
-	public:
-	  void setetxt() {
-	     std::cout << "mac TextBox created" << std::endl;
-	  }
+class MacTextBox : public ITextBox
+{
+public:
+   void setetxt()
+   {
+      std::cout << "mac TextBox created" << std::endl;
+   }
 };
 
-class WinTextBox : public ITextBox {
-	public:
-	  void setetxt() {
-	     std::cout << "win TextBox created" << std::endl;
-	  }
+class WinTextBox : public ITextBox
+{
+public:
+   void setetxt()
+   {
+      std::cout << "win TextBox created" << std::endl;
+   }
 };
 
-class GUIFactory{
-   public:
-     virtual ~GUIFactory() {}
-     virtual IButton* createButton() = 0;
-     virtual ITextBox* createTextBox() =0;
+class GUIFactory
+{
+public:
+   virtual ~GUIFactory() {}
+   virtual IButton *createButton() = 0;
+   virtual ITextBox *createTextBox() = 0;
 };
 
-class MacFactory : public GUIFactory{
-    public:
-      IButton* createButton(){
-         return new MacButton();
-      } 
-      ITextBox* createTextBox() {
-        return new MacTextBox();
+class MacFactory : public GUIFactory
+{
+public:
+   IButton *createButton()
+   {
+      return new MacButton();
+   }
+   ITextBox *createTextBox()
+   {
+      return new MacTextBox();
+   }
+};
+
+class WinFactory : public GUIFactory
+{
+public:
+public:
+   IButton *createButton()
+   {
+      return new WinButton();
+   }
+   ITextBox *createTextBox()
+   {
+      return new WinTextBox();
+   }
+};
+
+class GUIAbstractFactory
+{
+public:
+   static GUIFactory *createFactory(string osType)
+   {
+      if (osType == "Mac")
+      {
+         return new MacFactory();
       }
-};
-
-class WinFactory : public GUIFactory{ 
-    public:
-     public:
-      IButton* createButton(){
-         return new WinButton();
-      } 
-      ITextBox* createTextBox() {
-        return new WinTextBox();
+      else if (osType == "Win")
+      {
+         return new WinFactory();
       }
+      return nullptr;
+   }
 };
 
-class GUIAbstractFactory{
-    public:
-       static GUIFactory* createFactory(string osType){
-           if(osType == "Mac"){
-            return new MacFactory();
-           }else if(osType == "Win"){
-            return new WinFactory();
-           }
-           return nullptr;
-       }
-};
+int main()
+{
+   string osType;
+   cin >> osType;
 
-int main() {
-    string osType;
-	cin >> osType;
-
-	GUIFactory* element = GUIAbstractFactory::createFactory(osType);
-    IButton* button = element->createButton();
-    button->press();
-    ITextBox* textBox = element->createTextBox();
-    textBox->setetxt();
+   GUIFactory *element = GUIAbstractFactory::createFactory(osType);
+   IButton *button = element->createButton();
+   button->press();
+   ITextBox *textBox = element->createTextBox();
+   textBox->setetxt();
 }
-
