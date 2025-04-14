3a5472f224a594424e4d565773044824b81e58f6
Balie Shalomi Pacholi <31888480+balipacholi@users.noreply.github.com>
2025-04-14 20:17:33 +0530

    Update main.cpp

--- Factory Method Design Pattern/Furniture/main.cpp
+++ Factory Method Design Pattern/Furniture/main.cpp
@@ -1,63 +1,77 @@
 #include <iostream>
 
-class FurnitureItem 
+class FurnitureItem
 {
- public:
-   virtual ~FurnitureItem() {} 
-   virtual void display() = 0; 
+public:
+	virtual ~FurnitureItem() {}
+	virtual void display() = 0;
 };
 
-class Chair : public FurnitureItem{
-	public:
-	  void display() override {
-	    std::cout << "I'm Chair class" << std::endl;
-	  }
+class Chair : public FurnitureItem
+{
+public:
+	void display() override
+	{
+		std::cout << "I'm Chair class" << std::endl;
+	}
 };
 
-class Sofa : public FurnitureItem{
-	public:
-	  void display() override {
-	    std::cout << "I'm Sofa class" << std::endl;
-	  }
+class Sofa : public FurnitureItem
+{
+public:
+	void display() override
+	{
+		std::cout << "I'm Sofa class" << std::endl;
+	}
 };
 
-class Table : public FurnitureItem{
-	public:
-	  void display() override {
-	     std::cout << "I'm Table class" << std::endl;
-	  }
+class Table : public FurnitureItem
+{
+public:
+	void display() override
+	{
+		std::cout << "I'm Table class" << std::endl;
+	}
 };
 
-class FurnitureFactory{
-	public:
-     virtual ~FurnitureFactory() {}
-	 virtual FurnitureItem* createFurniture() = 0;
+class FurnitureFactory
+{
+public:
+	virtual ~FurnitureFactory() {}
+	virtual FurnitureItem *createFurniture() = 0;
 };
 
-class ChairFactory : public FurnitureFactory {
-	public:
-	  FurnitureItem* createFurniture() override {
-	    return new Chair();
-	 }
+class ChairFactory : public FurnitureFactory
+{
+public:
+	FurnitureItem *createFurniture() override
+	{
+		return new Chair();
+	}
 };
 
-class SofaFactory : public FurnitureFactory {
-	public:
-	  FurnitureItem* createFurniture() override {
-	    return new Sofa();
-	 }
+class SofaFactory : public FurnitureFactory
+{
+public:
+	FurnitureItem *createFurniture() override
+	{
+		return new Sofa();
+	}
 };
 
-class TableFactory : public FurnitureFactory {
-	public:
-	 FurnitureItem* createFurniture() override {
-	  return new Table();
-	 }
+class TableFactory : public FurnitureFactory
+{
+public:
+	FurnitureItem *createFurniture() override
+	{
+		return new Table();
+	}
 };
 
-int main(){
-	FurnitureFactory* sofaFactory = new SofaFactory();
-	FurnitureItem* sofa = sofaFactory->createFurniture();
+int main()
+{
+	FurnitureFactory *sofaFactory = new SofaFactory();
+	FurnitureItem *sofa = sofaFactory->createFurniture();
 	sofa->display();
 	delete sofaFactory;
 	delete sofa;
